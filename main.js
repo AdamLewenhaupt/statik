8d50c048eabc08fda8c4e3ae9ccd1eb1392b00bb
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2015-05-06 20:17:02 +0200

    added pimp animation 1

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
+function createAnimation() {
+	return setInterval(function(){
+		n = 8;
+		width = sWidth / n;
+		height = sHeight / n;
+
+		x = Math.floor(Math.random() * n) * width;
+		y = Math.floor(Math.random() * n) * height;
+
+		$effect = $("<div class='effect' />")
+			.offset({ top: y, left: x })
+			.width(width).height(height)
+			.appendTo($(".background-filter"));
+
+		$effect.delay(700).queue(function(next){ $(this).addClass('visible'); next(); })
+			.delay((Math.random() + 1) * 1000).queue(function(next){ $(this).removeClass('visible'); next(); })
+			.delay(700).queue(function(next){ $(this).remove(); next(); }); 
+
+	}, Math.random() * 100);
+}
+
+
 $(document).ready(function () {
+
+	sWidth = $(window).width();
+	sHeight = $(window).height();
+
+	intervalID = createAnimation();
+
 
 	fn = function (index) {
 
 		$filter = $(".background-filter");
 
 		if(index == 2) {
+			clearInterval(intervalID);
+			$(".effekt").removeClass('visible').delay(700).queue(function(next) { $(this).remove(); next(); });
+
 			$filter.addClass("dark").removeClass("light");
 		} else if (index == 1) {
+			clearInterval(intervalID);
 			$filter.removeClass('dark');
+			intervalID = createAnimation();
 		} else if (index == 3) {
 			$filter.addClass('light').removeClass('dark');
 		} else if (index == 4) {
